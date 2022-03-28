a21286570fb6fef21bdd3ba511f1c8a54b31afd0
nguyenchithanh113 <55727999+nguyenchithanh113@users.noreply.github.com>
2022-03-28 21:54:09 +0700

    first commit

---
+++ Assets/Planet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    [SerializeField] MeshFilter meshFilter;
+    TerrainFace TerrainFace;
+    void Start()
+    {
+        CreateFace();
+    }
+    void CreateFace()
+    {
+        TerrainFace = new TerrainFace(Vector3.right*2 , 4);
+        meshFilter.mesh = TerrainFace.ConstructMesh();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawLine(transform.position, TerrainFace.localUp,Color.green);
+        Debug.DrawLine(transform.position, TerrainFace.axisA, Color.red );
+        Debug.DrawLine(transform.position, TerrainFace.axisB, Color.blue);
+    }
+}
---
+++ Assets/TerrainFace.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainFace 
+{
+    public int resolution;
+    public Vector3 localUp;
+    public Vector3 axisA;
+    public Vector3 axisB;
+    public TerrainFace(Vector3 _up, int _resolution)
+    {
+        resolution = _resolution;
+        localUp = _up;
+        axisA = new Vector3(localUp.y, localUp.z, localUp.x);
+        axisB = Vector3.Cross(localUp, axisA);
+        axisB = axisB.normalized * localUp.magnitude;
+    }
+
+    public Mesh ConstructMesh()
+    {
+        Vector3[] verts = new Vector3[resolution * resolution];
+        int[] tris = new int[(resolution - 1) * (resolution - 1) * 2 * 3];
+        Vector2[] uvs = new Vector2[verts.Length];
+
+        Vector2[,] testArr = new Vector2[resolution, resolution];
+
+        int i = 0;
+        int trisIndex = 0;
+        int vertIndex = 0;
+        for(int y = 0; y < resolution; y++)
+        {
+            for(int x = 0; x < resolution; x++)
+            {
+                Vector2 percent = new Vector2(x, y) / (resolution-1);
+                Vector3 pointInFace = (localUp - axisA - axisB) + axisA * 2 * percent.x + axisB*2*percent.y ;
+                //Vector3 pointInFace = localUp + (percent.x - 0.5f) * axisA * 2 + (percent.y - 0.5f) * axisB * 2;
+                //Debug.Log(pointInFace);
+                verts[vertIndex] = pointInFace;
+                uvs[vertIndex] = percent;
+                if(x != resolution - 1 && y != resolution - 1)
+                {
+                    tris[trisIndex] = x + resolution * y;
+                    tris[trisIndex + 1] = x + 1 + resolution * y;
+                    tris[trisIndex + 2] = x + 1 + resolution * (y + 1);
+
+                    tris[trisIndex + 3] = x + resolution * y;
+                    tris[trisIndex + 4] = x + 1 + resolution * (y + 1);
+                    tris[trisIndex + 5] = x + resolution * (y + 1);
+                    trisIndex += 6;
+                    
+                }
+                if(x!=0 && y != 0)
+                {
+                    
+                }
+                vertIndex += 1;
+            }
+        }
+        Mesh mesh = new Mesh();
+        mesh.vertices = verts;
+        mesh.triangles = tris;
+        mesh.uv = uvs;
+        return mesh;
+    }
+}
